b274b8ce12dea4a80dc3aa4dcb993378b67b3575
liuhui-bupt <1968109705@qq.com>
2023-10-21 18:04:16 +0800

    linklist

---
+++ dlnode.h
@@ -0,0 +1,26 @@
+#ifndef DLNode_H
+#define DLNode_H
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsItem>
+#include <QBrush>
+#include "myarrowitem.h"
+class DLinkList;
+class DLNode
+{
+public:
+    DLNode(QString dt, DLNode *nt);         //构造函数
+    ~DLNode();                              //析构函数
+    void setNodeStatus(QBrush brush);
+    void removeAll(QGraphicsScene *scene);
+protected:
+    QString data;               //节点数据域
+    DLNode * next, *prior;      //节点指针域
+    //节点图形项
+    QGraphicsRectItem * valueRect, *pointerRect, *prePointerRect;
+    QGraphicsTextItem * valueText, *pointerText, *prePointerText;
+    std::vector<MyArrowItem*> arrowVector, preArrowVector;
+public:
+    friend class DLinkList;
+};
+#endif // DLNode_H
